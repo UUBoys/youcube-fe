214a89f6325ca6bc0de52f8d11c3e69b074cd45e
Matěj Šela <matejsela@gmail.com>
2023-04-06 13:31:12 +0200

    bug fix

--- youcube-fe/src/modules/common/components/NavBar.tsx
+++ youcube-fe/src/modules/common/components/NavBar.tsx
@@ -26,7 +26,7 @@ const NavBar = () => {
                 </form>
 
                 <div className="cursor-pointer">
-                    <Link href={user?.user ? 'profile' : '/auth/signin'} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                    <Link href={user?.user ? '/profile' : '/auth/signin'} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                         <svg className="h-12" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="PermIdentityIcon"><path d="M12 5.9c1.16 0 2.1.94 2.1 2.1s-.94 2.1-2.1 2.1S9.9 9.16 9.9 8s.94-2.1 2.1-2.1m0 9c2.97 0 6.1 1.46 6.1 2.1v1.1H5.9V17c0-.64 3.13-2.1 6.1-2.1M12 4C9.79 4 8 5.79 8 8s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 9c-2.67 0-8 1.34-8 4v3h16v-3c0-2.66-5.33-4-8-4z"></path></svg>
                     </Link>
                 </div>
