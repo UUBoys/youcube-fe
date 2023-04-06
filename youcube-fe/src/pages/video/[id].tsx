2f12d482824a3a35f2ca4ee161f3ec16f2c1941c
Matěj Šela <matejsela@gmail.com>
2023-04-06 15:36:08 +0200

    added title and description

--- youcube-fe/src/pages/video/[id].tsx
+++ youcube-fe/src/pages/video/[id].tsx
@@ -1,4 +1,5 @@
-import { GetVideoMutation } from '@/modules/mutations/VideoMutations';
+import Thumbnail from '@/modules/common/components/Thumbnail';
+import { GetVideoMutation, GetVideosMutation } from '@/modules/mutations/VideoMutations';
 import { useRouter } from 'next/router';
 import React from 'react'
 
@@ -10,26 +11,33 @@ const Video = () => {
     ? GetVideoMutation(typeof id === 'string' ? id : id[0])
     : { data: null, isLoading: true, error: null };
 
-  console.log(video)
+
   if (isLoading) return (
     <div>Loading...</div>
   )
 
   if (video) return (
-    <div className='w-full h-full min-h-screen mt-16 bg-white'>
-      <h1>{video.title}</h1>
-      <div className='h-screen'>
-        <iframe
-          width="75%"
-          height="70%"
-          src={`${video.url}?autoplay=1`}
-          title={video.title}
-          frameBorder="0"
-          className="rounded-lg"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
+    <div className='w-full h-full min-h-screen mt-16 bg-white text-gray-600'>
+      <div className='flex w-full h-screen flex-row'>
+        <div className='h-screen w-full'>
+          <iframe
+            width="75%"
+            height="70%"
+            src={`${video.url}?autoplay=1`}
+            title={video.title}
+            frameBorder="0"
+            className="rounded-lg"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+          <h1 className='text-4xl p-3'>{video.title}</h1>
+          <hr className='w-3/4' />
+          <p className='p-3'>
+            {video.description}
+          </p>
+        </div>
       </div>
+
     </div>
   )
 
