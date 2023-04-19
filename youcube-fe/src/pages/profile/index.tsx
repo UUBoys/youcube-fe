ea575c85a3088bec77a7e023b4b135de12b31d10
Daniel Seiner <51313652+dannyseiner@users.noreply.github.com>
2023-04-19 15:47:58 +0200

    Profile new-design, create re-designed

--- youcube-fe/src/pages/profile/index.tsx
+++ youcube-fe/src/pages/profile/index.tsx
@@ -20,22 +20,49 @@ const Profile = () => {
   };
 
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center bg-white p-6 pt-16 text-black">
-      <h2 className="mb-3 text-3xl">Profile - {user?.user?.name}</h2>
-      <button
-        type="button"
-        className="mr-2 mb-2 rounded-lg bg-gray-800 px-5 py-2.5 text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-        onClick={logout}
-      >
-        Logout
-      </button>
-      <button
-        type="button"
-        className="mr-2 mb-2 rounded-lg bg-gray-800 px-5 py-2.5 text-sm font-medium text-white hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-        onClick={uploadVideo}
-      >
-        Upload video
-      </button>
+    <div className="flex h-screen w-full flex-col items-center mt-10 bg-white p-6 pt-16 text-black">
+      <div className="w-full">
+        <div className="p-8 bg-white mt-24 rounded-xl shadow-[0px_7px_29px_0px_rgba(0,0,0,0.1)]">
+          <div className="grid grid-cols-1 md:grid-cols-3">
+            <div className="grid grid-cols-3 text-center order-last md:order-first mt-20 md:mt-0">
+              <div>
+                <p className="font-bold text-gray-700 text-xl">22</p>
+                <p className="text-gray-400">Videos</p>
+              </div>
+              <div>
+                <p className="font-bold text-gray-700 text-xl">10</p>
+                <p className="text-gray-400">Likes</p>
+              </div>
+              <div>
+                <p className="font-bold text-gray-700 text-xl">89</p>
+                <p className="text-gray-400">Comments</p>
+              </div>
+            </div>
+            <div className="relative">
+              <div className="w-48 h-48 bg-indigo-100 mx-auto rounded-full shadow-2xl absolute inset-x-0 top-0 -mt-24 flex items-center justify-center text-indigo-500">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-24 w-24" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
+                </svg>
+              </div>
+            </div>
+            <div className="space-x-8 flex justify-between mt-32 md:mt-0 md:justify-center">
+              <button className="text-white py-2 px-4 uppercase rounded bg-red-500 hover:bg-red-500 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
+                Subscribe
+              </button>
+              <button className="text-white py-2 px-4 uppercase rounded bg-gray-700 hover:bg-gray-800 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
+                Report
+              </button>
+            </div>
+          </div>
+          <div className="mt-20 text-center border-b pb-12">
+            <h1 className="text-4xl font-medium text-gray-700">{user?.user?.name}</h1>
+            <p className="mt-8 text-gray-500">{"{bio}"}</p>
+          </div>
+          <div className="mt-12 flex flex-col justify-center">
+            {"{videos}"}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
